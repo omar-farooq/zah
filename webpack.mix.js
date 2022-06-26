7b6d9dcac29c5dd64521a60d624aeb458c6d6780
Omar Farooq <wavygravy@outlook.com>
2022-06-26 21:12:59 +0100

    Configured Breeze starter pack with auth and vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
+    .postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')])
+    .sourceMaps()
+    .alias({
+        '@': 'resources/js',
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
