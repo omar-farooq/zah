cea6418e7230d32ea6f407f07567c3b7dd6ea41e
Omar Farooq <wavygravy@outlook.com>
2024-09-09 02:04:00 +0100

    maintenance request form input fixes

--- resources/js/Components/RequestFormInput.jsx
+++ resources/js/Components/RequestFormInput.jsx
@@ -14,7 +14,7 @@ export default function RequestFormInput
     {
     return (
         <input 
-            className={`border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600 ${additionalClasses}`}
+            className={`border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600 placeholder:text-slate-400 placeholder:italic ${additionalClasses}`}
             type={type} 
             name={name} 
             value={value}
